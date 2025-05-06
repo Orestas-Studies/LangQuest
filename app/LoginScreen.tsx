f25a6458b4517b4d8c92e8df287c7cbc3ba7b170
Your Name <you@example.com>
2025-05-06 17:45:27 +0300

    Pridetas Full Google Auth Loggin

--- app/LoginScreen.tsx
+++ app/LoginScreen.tsx
@@ -2,14 +2,52 @@ import React from 'react';
 import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 import { useRouter } from 'expo-router';
 import { LinearGradient } from 'expo-linear-gradient';
+import * as WebBrowser from 'expo-web-browser'
+import { useOAuth, useUser } from '@clerk/clerk-expo'
+import * as Linking from 'expo-linking'
+
+export const useWarmUpBrowser = () => {
+  React.useEffect(() => {
+    // Warm up the android browser to improve UX
+    // https://docs.expo.dev/guides/authentication/#improving-user-experience
+    void WebBrowser.warmUpAsync()
+    return () => {
+      void WebBrowser.coolDownAsync()
+    }
+  }, [])
+}
+
+WebBrowser.maybeCompleteAuthSession()
 
 const LoginScreen = () => {
   const router = useRouter();
 
-  const handleLoginWithGoogle = () => {
-    // Handle Google login here
-    console.log('Logging in with Google');
-  };
+  useWarmUpBrowser()
+
+  const { startOAuthFlow } = useOAuth({ strategy: 'oauth_google' })
+
+  const handleLoginWithGoogle = React.useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } = await startOAuthFlow({
+        redirectUrl: Linking.createURL('/gameScreen', { scheme: 'myapp' }),
+      })
+
+      // If sign in was successful, set the active session
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId })
+      } else {
+        // Use signIn or signUp returned from startOAuthFlow
+        // for next steps, such as MFA
+      }
+    } catch (err) {
+      // See https://clerk.com/docs/custom-flows/error-handling
+      // for more info on error handling
+      console.error(JSON.stringify(err, null, 2))
+    }
+  }, [])
+
+  const { user } = useUser();
+  console.log(user?.firstName);
 
   const handleLoginWithFacebook = () => {
     // Handle Facebook login here
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,23 @@
 import '../global.css';
 
 import { Stack } from 'expo-router';
+import { ClerkProvider } from '@clerk/clerk-expo'
+
+
+const myApiKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+if (!myApiKey) {
+  throw new Error ('EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY is not defined')
+}
 
 export default function Layout() {
   return (
+    <ClerkProvider publishableKey={myApiKey}>
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="gameScreen" options={{ headerShown: true }} />
       <Stack.Screen name="profileScreen" options={{ headerShown: false }} />
     </Stack>
+    </ClerkProvider>
   );
 }
